353a491a1bfabe7ae308377754eab7c4aadf0e86
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-12-13 11:16:20 -0800

    fix packages, working

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
-import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+import Notifications from './Notifications'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
-  render(){
+  render() {
+    
+    // console.log(this.props);
+    const { projects } = this.props;
+    
     return (
       <div className="dashboard container">
         <div className="row">
-
           <div className="col s12 m6">
-              <ProjectList/>
+            <ProjectList projects={projects} />
           </div>
-
           <div className="col s12 m5 offset-m1">
-              <Notifications />
+            <Notifications />
           </div>
         </div>
       </div>
@@ -21,4 +24,10 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 
-const store = createStore();
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
