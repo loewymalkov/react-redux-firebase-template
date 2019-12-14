602ad834fc2fc652069e26345b10c2e3260a960e
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-12-13 16:04:53 -0800

    add actions and refactor

--- marioplan/src/components/projects/CreateProject.js
+++ marioplan/src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { createProject } from '../../store/actions/projectActions'
+import { connect } from 'react-redux'
 
 class CreateProject extends Component {
   state = {
@@ -14,7 +16,8 @@ class CreateProject extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    // console.log(this.state);
+    this.props.createProject(this.state)
   }
   
   render() {
@@ -31,7 +34,7 @@ class CreateProject extends Component {
             <textarea className="textarea" id="content" onChange={this.handleChange}/>
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">CREATE</button>
+            <button className="btn pink lighten-1 z-depth-0">Create</button>
           </div>
         </form>
       </div>
@@ -39,4 +42,10 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
